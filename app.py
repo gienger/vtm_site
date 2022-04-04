9ea12c47098acdcd48b60bc3865cceffdd6bdb31
gienger <alec-gienger@uiowa.edu>
2022-04-04 16:24:11 -0500

    Estimate almost done

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from configparser import MissingSectionHeaderError
 from flask import Flask
 from flask import render_template
 
@@ -13,7 +14,21 @@ def about():
 
 @app.route('/estimate')
 def estimate():
-    return render_template('estimate.html', pageTitle='Estimate')
+    if request.method == 'POST':
+        form = request.form
+        radius = float(form['radius'])
+        height = float(form['height'])
+        pi= 3.14
+        tank_top= pi*radius^2
+        tank_sides= 2(pi(radius*height))
+        total_area= tank_top+tank_sides
+        area_sqft= total_area/144
+        material_cost= area_sqft*25
+        labor_cost= area_sqft*15
+        total_estimate= material_cost+labor_cost
+        print(total_estimate)
+        return render_template('estimate.html', pageTitle='Estimate', estimate=total_estimate)
+    return render_template('estimate.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
